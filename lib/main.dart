54bd1de2897bf1d40fa3a6bbc6aa5bb1071ff46d
Amin Sheikoleslami <amin.sheikoleslami@savestrike.de>
2025-05-12 05:33:12 +0200

    Add weather page with city search functionality and detailed weather display. Introduce new widgets for loading, error handling, and weather details. Update main.dart to reflect new screen structure and adjust .gitignore to include app/.cxx directory.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'providers/weather_provider.dart';
-import 'screens/weather_page/weather_page.dart';
+import 'screens/weather/weather_page.dart';
 
 void main() async {
   // Load environment variables from .env file
